74a580c25864b6d63894911913eabbbdb02a44ae
jeffery.raimundo@medaire.com <jeff_pepper23@yahoo.com>
2019-03-08 18:56:07 -0700

    Base HTML written starting Jquery

---
+++ Assets/Logic/logic.js
@@ -0,0 +1,54 @@
+$(document).ready(function() {
+
+    //Set up object storing manager properties 
+    var medAireManagers = {
+
+        "Diann": {
+            name: "Diann \"the Dream\" Weaverling",
+            image: "",
+            audio: "",
+            life: 100,
+            attack: 10,
+            returnAttack: 13,
+        },
+
+
+        "Timur": {
+            name: "Timur \"The Siberian Express\" Dury",
+            image: "",
+            audio: "",
+            life: 140,
+            attack: 15,
+            returnAttack: 10,
+
+        },
+            
+
+        "Will": {
+            name: "\"wild\" Will Mason",
+            image: "",
+            audio: "",
+            life: 120,
+            attack: 8,
+            returnAttack: 8,
+        },
+    
+        "Gerri": {
+            name: "Gerri \"The Dragon\" Kania",
+            image: "",
+            audio: "",
+            life: 120,
+            attack: 8,
+            returnAttack: 8,
+
+        },
+    };
+
+    //Declare global variables
+    var villians = [];
+    var hero = Null;
+    var opponent = Null;
+    
+    //Get fighter images to the screen for player selection
+    
+});
